4eab68a1ec8483620202a5a0572ea8f3889de83a
misoi <cytnhia.chepkemoi@gmail.com>
2016-06-04 20:55:53 +0300

    validated the log in form

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from flask import Flask
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] !='admin' or request.form['password'] != 'admin':
+            error = 'Invalid username or password'
+        else:
+            return redirect(url_for(''))
 #    return "Hello, World!"
-    return render_template("index.html")
+    return render_template("index.html", error=error)
+
