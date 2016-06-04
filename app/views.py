c71ca6e6bc9fac50fd253884c0f8960afd1f824d
misoi <cytnhia.chepkemoi@gmail.com>
2016-06-04 22:43:02 +0300

    added signup form and the form details

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash, session
 from flask import Flask
+from functools import wraps
 
 @app.route('/index')
 @app.route('/index', methods=['GET', 'POST'])
@@ -10,7 +11,19 @@ def index():
         if request.form['username'] !='admin' or request.form['password'] != 'admin':
             error = 'Invalid username or password'
         else:
-            return redirect(url_for(''))
+            session['logged_in'] = True
+            flash("You have successfully logged in!")
+            return redirect(url_for('logout'))
 #    return "Hello, World!"
     return render_template("index.html", error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash("you have just logged out")
+    return redirect(url_for('index'))
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 
